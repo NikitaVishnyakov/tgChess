d1d8998ef1407856858615e86cef9a83a3ed9859
NikitaVishnyakovInsider <74612287+NikitaVishnyakovInsider@users.noreply.github.com>
2024-12-23 23:46:40 +0200

    origin

---
+++ main.py
@@ -0,0 +1,32 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+
+# Команда /play
+from config import BOT_TOKEN
+
+
+async def play(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # URL вашего хостинга с шахматным приложением
+    web_app_url = "https://ваш-домен.com/chess"
+
+    # Кнопка запуска Web App
+    keyboard = [
+        [InlineKeyboardButton("Играть в шахматы", web_app=WebAppInfo(url=web_app_url))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Нажмите кнопку ниже, чтобы начать игру:", reply_markup=reply_markup)
+
+
+# Основной код
+def main():
+    TOKEN = BOT_TOKEN
+
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("play", play))
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
